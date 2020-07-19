c4a78e57fa7d0605cb9cee24b8bb9d87b9565bd2
sjfkai <sjfkai@163.com>
2020-07-19 13:13:26 +0800

    feat: update donate module

--- src/components/Donate.js
+++ src/components/Donate.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
-import { Row, Col, Button, Popover } from 'antd'
-import { HeartFilled, RedEnvelopeOutlined } from '@ant-design/icons'
+import { Popover } from 'antd'
+import { RedEnvelopeOutlined } from '@ant-design/icons'
 
-import paypalImg from '../assets/paypal.png'
 import wechatImg from '../assets/wechat.jpg'
 import alipayImg from '../assets/alipay.jpeg'
 import styles from './Donate.module.css'
@@ -13,7 +12,7 @@ class Donate extends Component {
     return (
       <div className={styles.donate}>
         <Popover content={content} title="感谢您的支持！ Thanks for your support!" placement="topRight">
-          <Button type="danger" ghost><RedEnvelopeOutlined /> 捐赠 Donate</Button>
+          <div className={styles.button}><RedEnvelopeOutlined /> 打赏 <RedEnvelopeOutlined /> </div>
         </Popover>
       </div>
     )
@@ -22,24 +21,18 @@ class Donate extends Component {
 
 const content = (
   <div className={styles['donate-content']}>
-    <Row gutter={50}>
-      <Col span={8}>
-        <p> <img src={paypalImg} alt="paypal QRCode" /> </p>
-        <p> <a href="https://www.paypal.me/sjfkai" target="_blank"  rel="noopener noreferrer">Pay for me</a> </p>
-      </Col>
-      <Col span={8}>
-        <img src={wechatImg} alt="wechat QRCode" />
-        <br />
-        <br />
-        <p> 打开微信[扫一扫] </p>
-      </Col>
-      <Col span={8}>
-        <p> <img src={alipayImg} alt="alipay QRCode" /> </p>
-        <p> 打开支付宝[扫一扫] </p>
-      </Col>
-    </Row>
-    <div style={{color: 'red'}}><HeartFilled />中国加油！世界加油！<HeartFilled /></div>
-    <div>感谢您的支持！本人会将疫情期间打赏通过各种渠道捐出。</div>
+    <div className={styles.qrcodes}>
+      <div className={styles.item}>
+        <img className={styles.image} src={wechatImg} alt="wechat QRCode" />
+        <p className={styles.desc} > 打开微信[扫一扫] </p>
+      </div>
+      <div className={styles['item']}>
+        <img className={styles.image} src={alipayImg} alt="alipay QRCode" />
+        <p className={styles.desc} > 打开支付宝[扫一扫] </p>
+      </div>
+    </div>
+    <br />
+    <div>本站完全免费，如果本站为你节约了时间，可否请我喝杯咖啡<span role="img" aria-label="Coffe">☕️</span>。非常感谢！</div>
   </div>
 );
 
